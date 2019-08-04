d5076b8e3f281c4a03b3d1b2c1816181aecc154a
Paweł Giżka <pawelgizka@gmail.com>
2019-08-04 10:26:22 +0200

    sbt paths fixed.

--- src/main/scala/io/scalac/tezos/translator/micheline/MichelineTranslator.scala
+++ src/main/scala/io/scalac/tezos/translator/micheline/MichelineTranslator.scala
@@ -1,5 +1,8 @@
 package io.scalac.tezos.translator.micheline
 
+import java.io.{File, PrintWriter}
+
+import scala.io.Source
 import scala.sys.process._
 import scala.util.{Failure, Success, Try}
 
@@ -8,13 +11,35 @@ object MichelineTranslator {
   val NODE_PATH = "node"
   val MICHELSON_LEXER_PATH = "src/main/scala/io/scalac/tezos/translator/micheline/MichelsonTranslator.js"
 
+  lazy val michelsonSource = Source.fromResource("Michelson.js").mkString
+  lazy val michelsonFile = {
+    val file = new File(".Michelson.js")
+    val writer = new PrintWriter(file)
+    writer.write(michelsonSource)
+    writer.close()
+    file.deleteOnExit()
+    file
+  }
+
+  lazy val translatorSource = Source.fromResource("MichelsonTranslator.js").mkString
+  lazy val translatorFile = {
+    val file = new File(".MichelsonTranslator.js")
+    val writer = new PrintWriter(file)
+    writer.write(translatorSource)
+    writer.close()
+    file.deleteOnExit()
+    file
+  }
+
   def michelsonToMicheline(input: String): Either[String, String] = {
     var errorLog: String = ""
     val logger = ProcessLogger(e =>
       errorLog += e + '\n'
     )
+    val michelsonSource = michelsonFile.getAbsolutePath
+    val translatorSource = translatorFile.getAbsolutePath
     Try {
-      Seq(NODE_PATH, MICHELSON_LEXER_PATH, input).!!(logger)
+      Seq(NODE_PATH, translatorSource, input).!!(logger)
     } match {
       case Failure(_) => Left(errorLog)
       case Success(value) => Right(value)
